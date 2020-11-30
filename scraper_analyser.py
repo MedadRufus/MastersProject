7a5c27affa20ef3ca4f038d1e2373cbb630a44a4
MedadRufus <mailto@medadnewman.co.uk>
2020-11-30 20:48:33 +0000

    Update scraper_analyser.py

--- scraper_analyser.py
+++ scraper_analyser.py
@@ -2,12 +2,22 @@ from pathlib import Path
 import datetime
 import json
 import pandas as pd
+from statistics import variance
 
 path = Path(r'C:\Users\Medad\PycharmProjects\MastersProject\datadump')
 
 
-df = pd.DataFrame()
+bikes = {}
 for e in path.glob('helbiz_*.json'):
     json_data = json.loads(e.read_text())
-    pd_temp = pd.DataFrame(json_data["data"]["bikes"])
-    print(pd_temp)
+
+    for i in json_data["data"]["bikes"]:
+        bikes[i["bike_id"]] = bikes.get(i["bike_id"], []) + [float(i["lat"])]
+
+
+    #pd_temp = pd.DataFrame(json_data["data"]["bikes"])
+    #print(pd_temp.head(4))
+
+for bike in bikes:
+    #print(bikes[bike])
+    print(variance(bikes[bike]))
