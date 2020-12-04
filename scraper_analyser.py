aa458c7eb9226db218b64ef74d582cf1281a99ff
MedadRufus <mailto@medadnewman.co.uk>
2020-12-04 18:14:26 +0000

    Update scraper_analyser.py

--- scraper_analyser.py
+++ scraper_analyser.py
@@ -10,17 +10,9 @@ bikes = {}
 for e in path.glob('helbiz_*.json'):
     json_data = json.loads(e.read_text())
     for i in json_data["data"]["bikes"]:
-        #print(i)
-        bikes[i["bike_id"]] = bikes.get(i["bike_id"], []) + [
-            {"lat": float(i["lat"]),
-             "lon": float(i["lon"]),
-             "in_use": int(i["in_use"]),
-             "idle_time": int(i["idle_time"]),
-             "is_reserved":int(i["is_reserved"])
-        }]
+        bikes[i["bike_id"]] = bikes.get(i["bike_id"], []) + [i]
 
 for bike in bikes:
     print(bike)
     print(*bikes[bike],sep="\n")
     print(variance([i["lon"] for i in bikes[bike]]))
-    # print(bikes[bike])
