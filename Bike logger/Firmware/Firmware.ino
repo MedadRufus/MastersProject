d38c908250c614266147defb18c08e6c0d0bb249
MedadRufus <mailto@medadnewman.co.uk>
2021-02-19 12:33:38 +0000

    put in standarsised template

--- Bike logger/Firmware/Firmware.ino
+++ Bike logger/Firmware/Firmware.ino
@@ -1,7 +1,59 @@
+/*********************************************************************
+** @source __ApplicationName__
+**
+** Main code for the Ebike logger
+**
+** @author Copyright (C) 2021  Medad Rufus Newman
+** @version v0.1
+********************************************************************/
+
+
+/* ==================================================================== */
+/* ========================== include files =========================== */
+/* ==================================================================== */
+
+
+/* Inclusion of system and local header files goes here */
+
+
+
+/* ==================================================================== */
+/* ============================ constants ============================= */
+/* ==================================================================== */
+
+/* #define and enum statements go here */
+
 #define LED_BUILTIN 33
 
+/* ==================================================================== */
+/* ======================== global variables ========================== */
+/* ==================================================================== */
+
+/* Global variables definitions go here */
+
+long lastTime = 0;
+
+/* ==================================================================== */
+/* ============================== data ================================ */
+/* ==================================================================== */
+
+/* Definition of datatypes go here */
+
+
+
+/* ==================================================================== */
+/* ==================== function prototypes =========================== */
+/* ==================================================================== */
+
+/* Function prototypes for public (external) functions go here */
 void blink_led(bool reset = false);
-long lastTime = 0; 
+
+
+/* @prog __ApplicationName ****************************************************
+**
+** Logs all data to SD card
+**
+******************************************************************************/
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
@@ -13,19 +65,23 @@ void loop() {
 }
 
 
+/* ==================================================================== */
+/* ============================ functions ============================= */
+/* ==================================================================== */
+
 
 void blink_led(bool reset)
 {
   const uint32_t LED_DELAY = 1000;
   static enum { LED_TOGGLE, WAIT_DELAY } state = LED_TOGGLE;
   static uint32_t timeLastTransition = 0;
- 
+
   if (reset)
-  { 
+  {
     state = LED_TOGGLE;
     digitalWrite(LED_BUILTIN, LOW);
   }
- 
+
   switch (state)
   {
     case LED_TOGGLE:   // toggle the LED
@@ -33,14 +89,14 @@ void blink_led(bool reset)
       timeLastTransition = millis();
       state = WAIT_DELAY;
       break;
- 
+
     case WAIT_DELAY:   // wait for the delay period
       if (millis() - timeLastTransition >= LED_DELAY)
       {
         state = LED_TOGGLE;
       }
       break;
- 
+
     default:
       state = LED_TOGGLE;
       break;
