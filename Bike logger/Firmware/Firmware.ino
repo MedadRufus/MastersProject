c92ddd7eccd7a9523fb6d4d3b0e2844705a45406
MedadRufus <mailto@medadnewman.co.uk>
2021-02-19 19:49:26 +0000

    create datastruct

--- Bike logger/Firmware/Firmware.ino
+++ Bike logger/Firmware/Firmware.ino
@@ -40,6 +40,40 @@ StateMachine poll_IN226(1, true);
 const int led1 = LED_BUILTIN;
 bool state1 = false;   // false is OFF, true is ON
 
+struct sensor_data
+{
+  /* GPS related */
+  uint32_t longitude;
+  uint32_t latitude;
+  uint32_t altitude;
+  uint32_t velocity;
+  uint32_t sats;
+
+  /* Acceleration/Gyroscope */
+  int16_t acc_x;
+  int16_t acc_y;
+  int16_t acc_z;
+  int16_t gyro_x;
+  int16_t gyro_y;
+  int16_t gyro_z;
+
+  /* Current voltage */
+  int32_t current_discharge;
+  int32_t voltage_discharge;
+  int32_t current_charge;
+  int32_t voltage_charge;
+
+  /* Temperature/pressure/humidity */
+  float temperature;
+  float pressure;
+  float humidity;
+
+  /* Other physical inputs */
+  int32_t pedal_rpm;
+  bool brake_state;
+  int32_t motor_speed;
+};
+
 /* ==================================================================== */
 /* ============================== data ================================ */
 /* ==================================================================== */
