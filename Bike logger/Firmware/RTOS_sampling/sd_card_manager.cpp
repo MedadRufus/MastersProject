01d3db5600fde00f18cf8413d89efa441b4df8bb
MedadRufus <mailto@medadnewman.co.uk>
2021-02-23 04:18:15 +0000

    make appendFileSimple()

--- Bike logger/Firmware/RTOS_sampling/sd_card_manager.cpp
+++ Bike logger/Firmware/RTOS_sampling/sd_card_manager.cpp
@@ -124,7 +124,12 @@ void SD_Manager::appendFile(fs::FS &fs, const char * path, const char * message)
   file.close();
 }
 
-void renameFile(fs::FS &fs, const char * path1, const char * path2) {
+
+void SD_Manager::appendFileSimple(const char * path, const char * message) {
+  appendFile(SD,path,message);
+}
+
+void SD_Manager::renameFile(fs::FS &fs, const char * path1, const char * path2) {
   Serial.printf("Renaming file %s to %s\n", path1, path2);
   if (fs.rename(path1, path2)) {
     Serial.println("File renamed");
--- Bike logger/Firmware/RTOS_sampling/sd_card_manager.h
+++ Bike logger/Firmware/RTOS_sampling/sd_card_manager.h
@@ -10,6 +10,7 @@ class SD_Manager {       // The class
     void removeDir(fs::FS &fs, const char * path);
     void writeFile(fs::FS &fs, const char * path, const char * message);
     void appendFile(fs::FS &fs, const char * path, const char * message);
+    void appendFileSimple( const char * path, const char * message);
     void renameFile(fs::FS &fs, const char * path1, const char * path2);
     void deleteFile(fs::FS &fs, const char * path);
     void testFileIO(fs::FS &fs, const char * path);
