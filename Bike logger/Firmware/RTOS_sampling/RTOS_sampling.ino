3e991215cd2322da721dc17be084260221444b3e
MedadRufus <medadrufus@gmail.com>
2021-03-01 18:19:06 +0000

    [doesn't work yet] pass ina226 role into task
    For some reason, only one role is executing, even when the one that is not executing is higher priority

--- Bike logger/Firmware/RTOS_sampling/RTOS_sampling.ino
+++ Bike logger/Firmware/RTOS_sampling/RTOS_sampling.ino
@@ -116,6 +116,15 @@ typedef enum
   CHARGE = 0,
   DISCHARGE
 }INA226_STATUS;
+/** Structure to initialize INA226 task */
+typedef struct {
+  INA226_STATUS ina226_status;
+} ina226_config_t;
+
+
+ina226_config_t charge_config = {.ina226_status = CHARGE};
+ina226_config_t discharge_config = {.ina226_status = DISCHARGE};
+
 SensorData sensor_data;
 static ms8607 m_ms8607;
 LSM6DS3 myIMU; //Default constructor is I2C, addr 0x6B
@@ -229,13 +238,21 @@ void setup() {
 
   xTaskCreatePinnedToCore(
     TaskManageINA226
-    ,  "TaskManageINA226"
+    ,  "TaskManageINA226_charge"
     ,  90000  // Stack size
-    ,  NULL
+    ,  &charge_config
     ,  3  // Priority
     ,  NULL
     ,  ARDUINO_RUNNING_CORE);
 
+  xTaskCreatePinnedToCore(
+    TaskManageINA226
+    ,  "TaskManageINA226_discharge"
+    ,  90000  // Stack size
+    ,  &discharge_config
+    ,  4  // Priority
+    ,  NULL
+    ,  ARDUINO_RUNNING_CORE);
 
   xTaskCreatePinnedToCore(
     TaskReadImu
@@ -359,8 +376,12 @@ void TaskManageGPS(void *pvParameters)
 
 void TaskManageINA226(void *pvParameters)
 {
-  (void) pvParameters;
+  //(void) pvParameters;
 
+  ina226_config_t our_config = *(ina226_config_t *) pvParameters;
+
+  Serial.print("charge config: ");
+  Serial.println(our_config.ina226_status);
 
   TickType_t xLastWakeTime;
   const TickType_t xFrequency = INA226_SAMPLE_INTERVAL;
