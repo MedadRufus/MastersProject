051b95a4c3771ab0f98e223bdb844672269c06b8
MedadRufus <medadrufus@gmail.com>
2021-03-01 18:23:21 +0000

    Revert "print role"
    This reverts commit cc2faa3f6195c777c2676d245200a3695187ba7b.

--- Bike logger/Firmware/RTOS_sampling/RTOS_sampling.ino
+++ Bike logger/Firmware/RTOS_sampling/RTOS_sampling.ino
@@ -619,10 +619,6 @@ void poll_ina226(INA226_STATUS ina226_status) {
   Serial.print(" ");
   
 
-  Serial.print("role: ");
-  Serial.print(ina226_status);
-  Serial.print(" ");
-
   sprintf(sprintfBuffer, "Bus_voltage[V]:%f, shunt_v_drop[mV]:%f, shunt_curr[mA]:%f, power[mW]:%f\n",
           busVoltage_V,
           shuntVoltage_mV,
