b27daced8bc1068d3fa41f813f639be04e9d9a26
MedadRufus <medadrufus@gmail.com>
2021-03-06 00:17:28 +0000

    blazing fast read, but errors are still present

--- Bike logger/Firmware/LSM6D_FifoExample/LSM6D_FifoExample.ino
+++ Bike logger/Firmware/LSM6D_FifoExample/LSM6D_FifoExample.ino
@@ -53,37 +53,40 @@
 
 LSM6DS3 myIMU;
 long lastTime = 0; //Simple local timer.
+char buffer_imu[400];
 
+uint16_t readings_in_one_go = 8;
+uint16_t bytes_in_one_reading = 12;
+uint16_t bytes_to_read = bytes_in_one_reading * readings_in_one_go;
 
-
-void setup( void ) {
+void setup(void)
+{
   Wire.begin(21, 22); // Acclerometer/gyro/temperature/pressure/humidity sensor
   Wire.setClock(400000);
 
-
   //Over-ride default settings if desired
-  myIMU.settings.gyroEnabled = 1;  //Can be 0 or 1
-  myIMU.settings.gyroRange = 2000;   //Max deg/s.  Can be: 125, 245, 500, 1000, 2000
+  myIMU.settings.gyroEnabled = 1;        //Can be 0 or 1
+  myIMU.settings.gyroRange = 2000;       //Max deg/s.  Can be: 125, 245, 500, 1000, 2000
   myIMU.settings.gyroSampleRate = 833;   //Hz.  Can be: 13, 26, 52, 104, 208, 416, 833, 1666
-  myIMU.settings.gyroBandWidth = 200;  //Hz.  Can be: 50, 100, 200, 400;
-  myIMU.settings.gyroFifoEnabled = 1;  //Set to include gyro in FIFO
-  myIMU.settings.gyroFifoDecimation = 1;  //set 1 for on /1
+  myIMU.settings.gyroBandWidth = 200;    //Hz.  Can be: 50, 100, 200, 400;
+  myIMU.settings.gyroFifoEnabled = 1;    //Set to include gyro in FIFO
+  myIMU.settings.gyroFifoDecimation = 1; //set 1 for on /1
 
   myIMU.settings.accelEnabled = 1;
-  myIMU.settings.accelRange = 16;      //Max G force readable.  Can be: 2, 4, 8, 16
-  myIMU.settings.accelSampleRate = 833;  //Hz.  Can be: 13, 26, 52, 104, 208, 416, 833, 1666, 3332, 6664, 13330
-  myIMU.settings.accelBandWidth = 200;  //Hz.  Can be: 50, 100, 200, 400;
-  myIMU.settings.accelFifoEnabled = 1;  //Set to include accelerometer in the FIFO
-  myIMU.settings.accelFifoDecimation = 1;  //set 1 for on /1
+  myIMU.settings.accelRange = 16;         //Max G force readable.  Can be: 2, 4, 8, 16
+  myIMU.settings.accelSampleRate = 833;   //Hz.  Can be: 13, 26, 52, 104, 208, 416, 833, 1666, 3332, 6664, 13330
+  myIMU.settings.accelBandWidth = 200;    //Hz.  Can be: 50, 100, 200, 400;
+  myIMU.settings.accelFifoEnabled = 1;    //Set to include accelerometer in the FIFO
+  myIMU.settings.accelFifoDecimation = 1; //set 1 for on /1
   myIMU.settings.tempEnabled = 1;
 
   //Non-basic mode settings
   myIMU.settings.commMode = 1;
 
   //FIFO control settings
-  myIMU.settings.fifoThreshold = 4095;  //Can be 0 to 4095 (16 bit bytes)
-  myIMU.settings.fifoSampleRate = 100;  //Hz.  Can be: 10, 25, 50, 100, 200, 400, 800, 1600, 3300, 6600
-  myIMU.settings.fifoModeWord = 6;  //FIFO mode.
+  myIMU.settings.fifoThreshold = 4095; //Can be 0 to 4095 (16 bit bytes)
+  myIMU.settings.fifoSampleRate = 400; //Hz.  Can be: 10, 25, 50, 100, 200, 400, 800, 1600, 3300, 6600
+  myIMU.settings.fifoModeWord = 6;     //FIFO mode.
   //FIFO mode.  Can be:
   //  0 (Bypass mode, FIFO off)
   //  1 (Stop when full)
@@ -91,13 +94,12 @@ void setup( void ) {
   //  4 (Bypass until trigger)
   //  6 (Continous mode)
 
-
-  Serial.begin(2000000);  // start serial for output
-  delay(1000); //relax...
+  Serial.begin(2000000); // start serial for output
+  delay(1000);           //relax...
   Serial.println("Processor came out of reset.\n");
 
   //Call .begin() to configure the IMUs
-  if ( myIMU.begin() != 0 )
+  if (myIMU.begin() != 0)
   {
     Serial.println("Problem starting the sensor with CS @ Pin 10.");
   }
@@ -113,13 +115,11 @@ void setup( void ) {
   Serial.print("Clearing out the FIFO...");
   myIMU.fifoClear();
   Serial.print("Done!\n");
-
 }
-
 
 void loop()
 {
-  float temp;  //This is to hold read data
+  float temp; //This is to hold read data
   uint16_t tempUnsigned;
 
   /* Poll the LSM6DS every second */
@@ -130,39 +130,35 @@ void loop()
     //Now loop until FIFO is empty.  NOTE:  As the FIFO is only 8 bits wide,
     //the channels must be synchronized to a known position for the data to align
     //properly.  Emptying the fifo is one way of doing this (this example)
-    while ( ( myIMU.fifoGetStatus() & 0x1000 ) == 0 ) {
+    while ((myIMU.fifoGetStatus() & 0x1000) == 0)
+    {
+      uint8_t data[bytes_to_read];
+      myIMU.readRegisterRegion(data, LSM6DS3_ACC_GYRO_FIFO_DATA_OUT_L, sizeof(data));
 
-      temp = myIMU.calcGyro(myIMU.fifoRead());
-      Serial.print(temp);
-      Serial.print(",");
+      for (int i = 0; i < readings_in_one_go; i++)
+      {
+        int16_t x_acc = convert(data, i * bytes_in_one_reading + 0);
+        int16_t y_acc = convert(data, i * bytes_in_one_reading + 2);
+        int16_t z_acc = convert(data, i * bytes_in_one_reading + 4);
+        int16_t x_gyro = convert(data, i * bytes_in_one_reading + 6);
+        int16_t y_gyro = convert(data, i * bytes_in_one_reading + 8);
+        int16_t z_gyro = convert(data, i * bytes_in_one_reading + 10);
 
-      temp = myIMU.calcGyro(myIMU.fifoRead());
-      Serial.print(temp);
-      Serial.print(",");
+        sprintf(buffer_imu, "imu,%d,%d,%d,%d,%d,%d\n",
+                x_acc,
+                y_acc,
+                z_acc,
+                x_gyro,
+                y_gyro,
+                z_gyro);
 
-      temp = myIMU.calcGyro(myIMU.fifoRead());
-      Serial.print(temp);
-      Serial.print(",");
-
-      temp = myIMU.calcAccel(myIMU.fifoRead());
-      Serial.print(temp);
-      Serial.print(",");
-
-      temp = myIMU.calcAccel(myIMU.fifoRead());
-      Serial.print(temp);
-      Serial.print(",");
-
-      temp = myIMU.calcAccel(myIMU.fifoRead());
-      Serial.print(temp);
-      Serial.print("\n");
-
-      //tempUnsigned = myIMU.fifoGetStatus();
-      //Serial.print("\nFifo Status 1 and 2 (16 bits): 0x");
-      //Serial.println(tempUnsigned, HEX);
-      //Serial.print("\n");
-
+        Serial.print(buffer_imu);
+      }
     }
-
   }
+}
 
+int16_t convert(uint8_t *bytes_source, uint16_t start)
+{
+  return (int16_t)bytes_source[start] | int16_t(bytes_source[start + 1] << 8);
 }
