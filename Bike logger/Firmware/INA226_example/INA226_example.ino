5f45a2f28fc23ca6c00a4f197c6cea29ce0c1551
MedadRufus <medadrufus@gmail.com>
2021-03-12 15:35:26 +0000

    set correct constants for shunt

--- Bike logger/Firmware/INA226_example/INA226_example.ino
+++ Bike logger/Firmware/INA226_example/INA226_example.ino
@@ -81,8 +81,8 @@
 ** Declare program constants, global variables and instantiate INA class                         **
 **************************************************************************************************/
 const uint32_t SERIAL_SPEED{115200};     ///< Use fast serial speed
-const uint32_t SHUNT_MICRO_OHM{100000};  ///< Shunt resistance in Micro-Ohm, e.g. 100000 is 0.1 Ohm
-const uint16_t MAXIMUM_AMPS{1};          ///< Max expected amps, clamped from 1A to a max of 1022A
+const uint32_t SHUNT_MICRO_OHM{2439};  ///< Shunt resistance in Micro-Ohm, e.g. 100000 is 0.1 Ohm
+const uint16_t MAXIMUM_AMPS{30};          ///< Max expected amps, clamped from 1A to a max of 1022A
 uint8_t        devicesFound{0};          ///< Number of INAs found
 INA_Class      INA;                      ///< INA class instantiation to use EEPROM
 // INA_Class      INA(0);                 ///< INA class instantiation to use EEPROM
@@ -110,7 +110,7 @@ void setup() {
   ** and for a 0.1Ohm resistor, and since no specific device is given as the 3rd parameter all   **
   ** devices are initially set to these values.                                                  **
   ************************************************************************************************/
-  Wire.begin(21,22);
+  Wire.begin(19,18);
   devicesFound = INA.begin(MAXIMUM_AMPS, SHUNT_MICRO_OHM);  // Expected max Amp & shunt resistance
   while (devicesFound == 0) {
     Serial.println(F("No INA device found, retrying in 10 seconds..."));
