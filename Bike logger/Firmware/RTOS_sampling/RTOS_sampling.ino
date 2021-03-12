6e42311cae5fc3595663db7d7b87f455cde26507
MedadRufus <medadrufus@gmail.com>
2021-03-12 19:49:01 +0000

    sample only ina226

--- Bike logger/Firmware/RTOS_sampling/RTOS_sampling.ino
+++ Bike logger/Firmware/RTOS_sampling/RTOS_sampling.ino
@@ -765,7 +765,7 @@ void poll_ina226(INA226_STATUS ina226_status)
           current_mA,
           power_mW);
 
-  //Serial.print(sprintfBuffer);
+  Serial.print(sprintfBuffer);
   xSemaphoreTake(SPI_SD_Mutex, portMAX_DELAY);
   sd_manager.appendFile(&data_file, sprintfBuffer);
   xSemaphoreGive(SPI_SD_Mutex); // release mutex
