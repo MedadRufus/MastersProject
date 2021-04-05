8c72091961e7f61c974d28be7eaf2b01fa037109
MedadRufus <medadrufus@gmail.com>
2021-04-05 19:18:20 +0100

    setup mcpwm_capture_signal_get_value();

--- Bike logger/Firmware/pulse_counter_rtos/pulse_counter_rtos.ino
+++ Bike logger/Firmware/pulse_counter_rtos/pulse_counter_rtos.ino
@@ -1,9 +1,13 @@
+#include "driver/mcpwm.h"
 
 
+
+const uint32_t SERIAL_SPEED = 115200; // Use fast serial speed 2 Mbits/s
 
 
 // the number of the LED pin
 const int ledPin = 27;
+const int MotorAPin = 35;
 
 
 
@@ -16,16 +20,26 @@ const int resolution = 8;
 const int dutyCycle = downtime_dutyCycle * 255 / 100;
 
 void setup() {
+  Serial.begin(SERIAL_SPEED);
+  Serial.println("=======================================================");
+
   // configure LED PWM functionalitites
   ledcSetup(ledChannel, freq, resolution);
 
   // attach the channel to the GPIO to be controlled
   ledcAttachPin(ledPin, ledChannel);
-
   ledcWrite(ledChannel, dutyCycle);
+
+
+  // Read pwm
+  mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0A, MotorAPin);
+  mcpwm_capture_enable(MCPWM_UNIT_0, MCPWM_SELECT_CAP0, MCPWM_NEG_EDGE, 0);
 }
 
 void loop() {
-  // increase the LED brightness
+  unsigned int number = mcpwm_capture_signal_get_value(MCPWM_UNIT_0, MCPWM_SELECT_CAP0);
+  Serial.print("PWM_input_value: ");
+  Serial.println(number);
+  delay(300);
 
 }
