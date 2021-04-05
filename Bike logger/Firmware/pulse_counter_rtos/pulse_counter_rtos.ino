a9418af05148515a8ada99f17bafc92ff7c96b26
MedadRufus <medadrufus@gmail.com>
2021-04-05 19:42:50 +0100

    fix imports

--- Bike logger/Firmware/pulse_counter_rtos/pulse_counter_rtos.ino
+++ Bike logger/Firmware/pulse_counter_rtos/pulse_counter_rtos.ino
@@ -37,6 +37,8 @@ const uint32_t SERIAL_SPEED = 115200; // Use fast serial speed 2 Mbits/s
 const int ledPin = 27;
 const int MotorAPin = 35;
 
+#include "soc/mcpwm_reg.h"
+#include "soc/mcpwm_struct.h"
 
 #define MCPWM_EN_CARRIER 0   //Make this 1 to test carrier submodule of mcpwm, set high frequency carrier parameters
 #define MCPWM_EN_DEADTIME 0  //Make this 1 to test deadtime submodule of mcpwm, set deadtime value and deadtime mode
