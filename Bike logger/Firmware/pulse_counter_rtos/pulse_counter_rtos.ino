7b31ec0f1e1fe13b01a27ff1686573120f4d6fb2
MedadRufus <medadrufus@gmail.com>
2021-04-05 19:32:42 +0100

    set pin numbers

--- Bike logger/Firmware/pulse_counter_rtos/pulse_counter_rtos.ino
+++ Bike logger/Firmware/pulse_counter_rtos/pulse_counter_rtos.ino
@@ -22,6 +22,21 @@
 #include "soc/rtc.h"
 #include "driver/mcpwm.h"
 #include "soc/mcpwm_periph.h"
+// setting PWM properties
+const int freq = 5;  // set this
+const int downtime_dutyCycle = 83; // set this
+
+const int ledChannel = 0;
+const int resolution = 8;
+const int dutyCycle = downtime_dutyCycle * 255 / 100;
+
+
+const uint32_t SERIAL_SPEED = 115200; // Use fast serial speed 2 Mbits/s
+
+// the number of the LED pin
+const int ledPin = 27;
+const int MotorAPin = 35;
+
 
 #define MCPWM_EN_CARRIER 0   //Make this 1 to test carrier submodule of mcpwm, set high frequency carrier parameters
 #define MCPWM_EN_DEADTIME 0  //Make this 1 to test deadtime submodule of mcpwm, set deadtime value and deadtime mode
