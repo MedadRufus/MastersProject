06c0bb96dd0b91438e80f5229636fc8cd488627c
MedadRufus <medadrufus@gmail.com>
2021-04-05 18:25:53 +0100

    put in simple pwm fade

--- Bike logger/Firmware/pulse_counter_rtos/pulse_counter_rtos.ino
+++ Bike logger/Firmware/pulse_counter_rtos/pulse_counter_rtos.ino
@@ -1,119 +1,31 @@
-/*********************************************************************
-** @source Ebike Blackbox datalogger
-**
-** Main code for the Ebike logger
-**
-** @author Copyright (C) 2021  Medad Rufus Newman
-** @version v0.1
-** Mutex tips from https://microcontrollerslab.com/arduino-freertos-mutex-tutorial-priority-inversion-priority-inheritance/
-********************************************************************/
+// the number of the LED pin
+const int ledPin = 27;  // 16 corresponds to GPIO16
 
-/* ==================================================================== */
-/* ========================== include files =========================== */
-/* ==================================================================== */
+// setting PWM properties
+const int freq = 5;
+const int ledChannel = 0;
+const int resolution = 8;
 
-/* Inclusion of system and local header files goes here */
+void setup(){
+  // configure LED PWM functionalitites
+  ledcSetup(ledChannel, freq, resolution);
 
-
-/* ==================================================================== */
-/* ============================ constants ============================= */
-/* ==================================================================== */
-
-/* #define and enum statements go here */
-#if CONFIG_FREERTOS_UNICORE
-#define ARDUINO_RUNNING_CORE 0
-#else
-#define ARDUINO_RUNNING_CORE 1
-#endif
-
-#define LED_BUILTIN 27
-#define BLINK_INTERVAL 100
-
-
-/* ==================================================================== */
-/* ======================== global variables ========================== */
-/* ==================================================================== */
-
-TaskHandle_t Handle_blink_Task;
-
-
-const uint32_t SERIAL_SPEED = 2000000; // Use fast serial speed 2 Mbits/s
-
-
-/* ==================================================================== */
-/* ============================== data ================================ */
-/* ==================================================================== */
-
-/* Definition of datatypes go here */
-
-/* ==================================================================== */
-/* ==================== function prototypes =========================== */
-/* ==================================================================== */
-
-/* Function prototypes for public (external) functions go here */
-
-/* ==================================================================== */
-/* ============================ functions ============================= */
-/* ==================================================================== */
-
-void setup()
-{
-
-
-  Serial.begin(SERIAL_SPEED);
-
-  start_tasks();
+  // attach the channel to the GPIO to be controlled
+  ledcAttachPin(ledPin, ledChannel);
 }
 
-void start_tasks()
-{
-  // Now set up tasks to run independently.
-  xTaskCreatePinnedToCore(
-    TaskBlink, "TaskBlink" // A name just for humans
-    ,
-    1024 // This stack size can be checked & adjusted by reading the Stack Highwater
-    ,
-    NULL, 1 // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
-    ,
-    &Handle_blink_Task, ARDUINO_RUNNING_CORE);
+void loop(){
+  // increase the LED brightness
+  for(int dutyCycle = 0; dutyCycle <= 255; dutyCycle++){   
+    // changing the LED brightness with PWM
+    ledcWrite(ledChannel, dutyCycle);
+    delay(15);
+  }
 
-
-  // Now the task scheduler, which takes over control of scheduling individual tasks, is automatically started.
-}
-
-void loop()
-{
-  // Empty. Things are done in Tasks.
-}
-
-/*--------------------------------------------------*/
-/*---------------------- Tasks ---------------------*/
-/*--------------------------------------------------*/
-
-
-/*
-  Blink
-  Turns on an LED on for BLINK_INTERVAL, then off for BLINK_INTERVAL, repeatedly.
-*/
-void TaskBlink(void *pvParameters) // This is a task.
-{
-  (void)pvParameters;
-  TickType_t xLastWakeTime;
-  const TickType_t xFrequency = BLINK_INTERVAL;
-
-  bool led_state = false; // false is OFF, true is ON
-
-  // initialize digital LED_BUILTIN on pin 13 as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, led_state ? HIGH : LOW);
-
-  // Initialise the xLastWakeTime variable with the current time.
-  xLastWakeTime = xTaskGetTickCount();
-  for (;;)
-  {
-    // Wait for the next cycle.
-    vTaskDelayUntil(&xLastWakeTime, xFrequency);
-    led_state = !led_state;
-    digitalWrite(LED_BUILTIN, led_state ? HIGH : LOW);
+  // decrease the LED brightness
+  for(int dutyCycle = 255; dutyCycle >= 0; dutyCycle--){
+    // changing the LED brightness with PWM
+    ledcWrite(ledChannel, dutyCycle);   
+    delay(15);
   }
 }
