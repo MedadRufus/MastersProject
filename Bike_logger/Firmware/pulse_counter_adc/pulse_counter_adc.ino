35e44dbcd707fa9917762d5670c9ca0030dcca1d
MedadRufus <medadrufus@gmail.com>
2021-04-17 01:59:24 +0100

    Create pulse_counter_adc.ino

---
+++ Bike_logger/Firmware/pulse_counter_adc/pulse_counter_adc.ino
@@ -0,0 +1,28 @@
+typedef enum{
+  POS = 0,
+  NEG,
+} edge_t;
+
+
+int low_threshold = 400;    //mV
+int high_threshold = 3500;  //mW
+
+bool is_edge(edge_t direction, int previous_v, int current_v)
+{
+
+}
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
