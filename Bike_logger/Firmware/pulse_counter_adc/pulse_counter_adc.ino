6e867b92b9236b339a5ccc692bb1aaf7bebaf60e
MedadRufus <medadrufus@gmail.com>
2021-04-19 00:22:22 +0100

    Auto format only

--- Bike_logger/Firmware/pulse_counter_adc/pulse_counter_adc.ino
+++ Bike_logger/Firmware/pulse_counter_adc/pulse_counter_adc.ino
@@ -49,37 +49,36 @@ typedef enum
 int low_threshold = 400;   //mV
 int high_threshold = 3500; //mV
 
-
 float previous_v = 0;
-  
 
 void i2sInit()
 {
-   i2s_config_t i2s_config = {
-    .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_ADC_BUILT_IN),
-    .sample_rate =  I2S_SAMPLE_RATE,              // The format of the signal using ADC_BUILT_IN
-    .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT, // is fixed at 12bit, stereo, MSB
-    .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
-    .communication_format = I2S_COMM_FORMAT_I2S_MSB,
-    .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
-    .dma_buf_count = 4,
-    .dma_buf_len = 8,
-    .use_apll = false,
-    .tx_desc_auto_clear = false,
-    .fixed_mclk = 0
-   };
-   i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
-   i2s_set_adc_mode(ADC_UNIT_1, ADC_INPUT);
-   i2s_adc_enable(I2S_NUM_0);
+  i2s_config_t i2s_config = {
+      .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_ADC_BUILT_IN),
+      .sample_rate = I2S_SAMPLE_RATE,               // The format of the signal using ADC_BUILT_IN
+      .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT, // is fixed at 12bit, stereo, MSB
+      .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
+      .communication_format = I2S_COMM_FORMAT_I2S_MSB,
+      .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
+      .dma_buf_count = 4,
+      .dma_buf_len = 8,
+      .use_apll = false,
+      .tx_desc_auto_clear = false,
+      .fixed_mclk = 0};
+  i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
+  i2s_set_adc_mode(ADC_UNIT_1, ADC_INPUT);
+  i2s_adc_enable(I2S_NUM_0);
 }
 
-void reader(void *pvParameters) {
+void reader(void *pvParameters)
+{
   uint32_t read_counter = 0;
   uint64_t read_sum = 0;
   // The 4 high bits are the channel, and the data is inverted
   uint16_t offset = (int)ADC_INPUT * 0x1000 + 0xFFF;
   size_t bytes_read;
-  while(1){
+  while (1)
+  {
     uint16_t buffer[1] = {0};
     i2s_read(I2S_NUM_0, &buffer, sizeof(buffer), &bytes_read, 15);
 
@@ -87,19 +86,17 @@ void reader(void *pvParameters) {
 
     float filteredval = f.filterIn((float)adc_value);
 
-    Serial.printf("%d, %f\n", adc_value,filteredval);
+    Serial.printf("%d, %f\n", adc_value, filteredval);
 
-    
-    bool is_edge_state = is_edge(NEG,previous_v,filteredval);
+    bool is_edge_state = is_edge(NEG, previous_v, filteredval);
     previous_v = filteredval;
 
     if (is_edge_state)
     {
-      Serial.printf("edge detected at %s",NTP.getTimeDateStringUs());
+      Serial.printf("edge detected at %s", NTP.getTimeDateStringUs());
     }
   }
 }
-
 
 /**
  * @brief Init ntp
@@ -165,28 +162,24 @@ bool is_edge(edge_t direction, float previous_v, float current_v)
   return false;
 }
 
-
-
-void setup() 
+void setup()
 {
   Serial.begin(115200);
 
   // ntp for timestamp
   init_ntp();
 
-
-  // Put a signal out on pin 
+  // Put a signal out on pin
 
   // configure LED PWM functionalitites
   ledcSetup(ledChannel, freq, resolution);
-  
+
   // attach the channel to the GPIO to be controlled
   ledcAttachPin(ledPin, ledChannel);
 
   // Generate PWM signal
   ledcWrite(ledChannel, dutyCycle);
 
-  
   // Initialize the I2S peripheral
   i2sInit();
   // Create a task that will read the data
@@ -195,5 +188,4 @@ void setup()
 
 void loop()
 {
-
 }
