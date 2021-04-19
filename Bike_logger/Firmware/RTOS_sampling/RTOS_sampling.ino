c512745fe3097595a748534927a518566c2051df
MedadRufus <medadrufus@gmail.com>
2021-04-19 18:46:13 +0100

    make separate file for heap analysis

--- Bike_logger/Firmware/RTOS_sampling/RTOS_sampling.ino
+++ Bike_logger/Firmware/RTOS_sampling/RTOS_sampling.ino
@@ -26,6 +26,7 @@
 #include "WifiConfig.h"
 #include "config.h"
 #include <bitset>
+#include "heap_analysis.hpp"
 
 #include "src/pulse_counter_adc/pulse_counter_adc.hpp"
 #include "src/pulse_counter_rtos/pulse_counter_rtos.hpp"
@@ -808,78 +809,4 @@ void init_ntp()
   WiFi.begin(YOUR_WIFI_SSID, YOUR_WIFI_PASSWD);
   NTP.setTimeZone(TZ_Etc_UTC);
   NTP.begin();
-}
-
-void heap_analysis()
-{
-  int x;
-  int measurement;
-
-  Serial.flush();
-  Serial.println("");
-  Serial.println("****************************************************");
-  Serial.print("Free Heap: ");
-  Serial.print(xPortGetFreeHeapSize());
-  Serial.println(" bytes");
-
-  Serial.print("Min Heap: ");
-  Serial.print(xPortGetMinimumEverFreeHeapSize());
-  Serial.println(" bytes");
-  Serial.flush();
-
-#if 0
-  Serial.println("****************************************************");
-  Serial.println("Task            ABS             %Util");
-  Serial.println("****************************************************");
-
-  //vTaskGetRunTimeStats(ptrTaskList); //save stats to char array
-  Serial.println(ptrTaskList); //prints out already formatted stats
-  Serial.flush();
-
-  Serial.println("****************************************************");
-  Serial.println("Task            State   Prio    Stack   Num     Core" );
-  Serial.println("****************************************************");
-
-  //vTaskList(ptrTaskList); //save stats to char array
-  Serial.println(ptrTaskList); //prints out already formatted stats
-  Serial.flush();
-
-  Serial.println("****************************************************");
-  Serial.println("[Stacks Free Bytes Remaining] ");
-#endif
-
-  measurement = uxTaskGetStackHighWaterMark(Handle_gps_Task);
-  Serial.print("Handle_gps_Task: ");
-  Serial.println(measurement);
-
-  measurement = uxTaskGetStackHighWaterMark(Handle_baroTask);
-  Serial.print("Handle_baroTask: ");
-  Serial.println(measurement);
-
-  measurement = uxTaskGetStackHighWaterMark(Handle_imu_Task);
-  Serial.print("Handle_imu_Task: ");
-  Serial.println(measurement);
-
-  measurement = uxTaskGetStackHighWaterMark(Handle_ina1_Task);
-  Serial.print("Handle_ina1_Task: ");
-  Serial.println(measurement);
-
-  measurement = uxTaskGetStackHighWaterMark(Handle_ina2_Task);
-  Serial.print("Handle_ina2_Task: ");
-  Serial.println(measurement);
-
-  measurement = uxTaskGetStackHighWaterMark(Handle_blink_Task);
-  Serial.print("Handle_blink_Task: ");
-  Serial.println(measurement);
-
-  measurement = uxTaskGetStackHighWaterMark(Handle_speed_Task);
-  Serial.print("Handle_speed_Task: ");
-  Serial.println(measurement);
-
-  measurement = uxTaskGetStackHighWaterMark(Handle_brake_Task);
-  Serial.print("Handle_brake_Task: ");
-  Serial.println(measurement);
-
-  Serial.println("****************************************************");
-  Serial.flush();
 }
