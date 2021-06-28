6b375add47f07f349f3fe3b863cd535b85479e65
MedadRufus <medadrufus@gmail.com>
2021-06-28 13:56:02 +0100

    Create influx_db_tests.py

---
+++ Bike_logger/Data_analysis/scratch-tests/influx_db_tests.py
@@ -0,0 +1,62 @@
+
+
+"""
+Connect to InfluxDB 2.0 - write data and query them
+"""
+
+from datetime import datetime
+
+from influxdb_client import Point, InfluxDBClient
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+"""
+Configure credentials
+"""
+influx_cloud_url = "https://westeurope-1.azure.cloud2.influxdata.com"
+influx_cloud_token = "dyG23pQt3PPZrYAAQFaNkk29fVPmWY8e7n1S7PH3FImF4_kv6iKF0R400a4XRZ7TvS8QpUBwjPol34rGiILwIg=="
+bucket = "medad's Bucket"
+org = "medad@medadnewman.co.uk"
+
+client = InfluxDBClient(url=influx_cloud_url, token=influx_cloud_token)
+try:
+    kind = 'temperature'
+    host = 'host1'
+    device = 'opt-123'
+
+    """
+    Write data by Point structure
+    """
+    point = Point(kind).tag('host', host).tag('device', device).field('value', 25.3).time(time=datetime.utcnow())
+
+    print(f'Writing to InfluxDB cloud: {point.to_line_protocol()} ...')
+
+    write_api = client.write_api(write_options=SYNCHRONOUS)
+    write_api.write(bucket=bucket, org=org, record=point)
+
+    print()
+    print('success')
+    print()
+    print()
+
+    """
+    Query written data
+    """
+    query = f'from(bucket: "{bucket}") |> range(start: -1d) |> filter(fn: (r) => r._measurement == "{kind}")'
+    print(f'Querying from InfluxDB cloud: "{query}" ...')
+    print()
+
+    query_api = client.query_api()
+    tables = query_api.query(query=query, org=org)
+
+    for table in tables:
+        for row in table.records:
+            print(f'{row.values["_time"]}: host={row.values["host"]},device={row.values["device"]} '
+                  f'{row.values["_value"]} °C')
+
+    print()
+    print('success')
+
+except Exception as e:
+    print(e)
+finally:
+    client.close()
